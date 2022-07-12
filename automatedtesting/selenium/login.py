70f9ad80fd467af5e4caeffd1a482328429f80f2
MaiQD <q.dat2710@gmail.com>
2022-07-12 14:32:06 +0700

    run login using selenium

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -2,6 +2,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
+def currentTime():
+    ts = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return (ts + '\t')
 
 # Start the browser and login with standard_user
 def login (user, password):
@@ -11,9 +14,17 @@ def login (user, password):
     options.add_argument("--headless") 
     driver = webdriver.Chrome(options=options)
     # driver = webdriver.Chrome()
-    print ('Browser started successfully. Navigating to the demo page to login.')
+    print (currentTime() + 'Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
-    print ('Navigating to the demo page to login success.')
+    print (currentTime() + 'Navigating to the demo page to login success.')
+    
+    # login
+    driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
+    driver.find_element_by_css_selector("input[id='password']").send_keys(password)
+    driver.find_element_by_id("login-button").click()
+    product_label = driver.find_element_by_css_selector("div[class='product_label']").text
+    assert "Products" in product_label
+    print(currentTime() + 'Login successfully as {:s} .'.format(user))
 
 
 login('standard_user', 'secret_sauce')
