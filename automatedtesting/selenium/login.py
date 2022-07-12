d2f6e363ad7f0569171d04d35e7fbf75a1279114
MaiQD <q.dat2710@gmail.com>
2022-07-12 11:24:54 +0700

    Base test with selenium

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -7,12 +7,14 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 def login (user, password):
     print ('Starting the browser...')
     # --uncomment when running in Azure DevOps.
-    # options = ChromeOptions()
-    # options.add_argument("--headless") 
-    # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
+    options = ChromeOptions()
+    options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=options)
+    # driver = webdriver.Chrome()
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
+    print ('Navigating to the demo page to login success.')
+
 
 login('standard_user', 'secret_sauce')
 
